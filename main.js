02dd66085f6c57117758de73fb49e998d39af95e
Diego Alves <diegoalvesdc12@gmail.com>
2022-03-26 18:44:08 -0300

    Fixed bugs

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 const button = document.querySelector('search-button')
 
 const apiUrl =
-  'https://api.hgbrasil.com/weather?format=json-cors&key=0277e90a&city_name='
-
-search_button.addEventListener('click', function () {
-  getInfos(search.value)
-})
+  'https://api.hgbrasil.com/weather?format=json-cors&key=413cf1fc&city_name='
 
 function getInfos(city) {
   axios
@@ -20,49 +16,74 @@ function getInfos(city) {
       sunrise.textContent = results.sunrise
       sunset.textContent = results.sunset
       changeImg(Number(results.condition_code))
-      console.log(Number(results.condition_code))
     })
     .catch(error => console.error(error))
 }
 
+search_button.addEventListener('click', function () {
+  getInfos(search.value)
+})
+
 function changeImg(code) {
-  if (code === 27 || 31 || 32) {
+  if (code == 27 || code == 31 || code == 32) {
     weather_img.src = './assets/images/ensolarado.svg'
-  } else if (code == 9 || 11 || 12 || 40 || 45) {
+  } else if (
+    code == 9 ||
+    code == 11 ||
+    code == 12 ||
+    code == 40 ||
+    code == 45
+  ) {
     weather_img.src = './assets/images/chuva-leve.svg'
   } else if (
     code == 0 ||
-    1 ||
-    2 ||
-    3 ||
-    4 ||
-    14 ||
-    21 ||
-    37 ||
-    38 ||
-    39 ||
-    47
+    code == 1 ||
+    code == 2 ||
+    code == 3 ||
+    code == 4 ||
+    code == 14 ||
+    code == 21 ||
+    code == 37 ||
+    code == 38 ||
+    code == 39 ||
+    code == 47
   ) {
     weather_img.src = './assets/images/tempestade.svg'
-  } else if (code === 16 || 46 || 41 || 42 || 13 || 15 || 43) {
+  } else if (
+    code == 16 ||
+    code == 46 ||
+    code == 41 ||
+    code == 42 ||
+    code == 13 ||
+    code == 15 ||
+    code == 43
+  ) {
     weather_img.src = './assets/images/neve.svg'
-  } else if (code === 19 || 20 || 22 || 23 || 24) {
+  } else if (
+    code == 19 ||
+    code == 20 ||
+    code == 22 ||
+    code == 23 ||
+    code == 24
+  ) {
     weather_img.src = './assets/images/ventania'
-  } else if (code === 8 || 10 || 17 || 18) {
+  } else if (code == 8 || code == 10 || code == 17 || code == 18) {
     weather_img.src = './assets/images/geada.svg'
-  } else if (code === 26 || 28 || 29 || 30) {
+  } else if (code == 26 || code == 28 || code == 29 || code == 30) {
     weather_img.src = './assets/images/nublado.svg'
-  } else if (code === 34 || 44) {
+  } else if (code == 34 || code == 44) {
     weather_img.src = './assets/images/ensolarado-nuvens.svg'
-  } else if (code === 5 || 6 || 7 || 35) {
+  } else if (code == 5 || code == 6 || code == 7 || code == 35) {
     weather_img.src = './assets/images/chuva-granizo.svg'
-  } else if (code === 25) {
+  } else if (code == 25) {
     weather_img.src = './assets/images/tempo-frio.svg'
-  } else if (code === 33) {
+  } else if (code == 33) {
     weather_img.src = './assets/images/estrelado.svg'
-  } else if (code === 36) {
+  } else if (code == 36) {
     weather_img.src = './assets/images/ar-quente.svg'
   } else {
     weather_img.src = './assets/images/ensolarado-nuvens.svg'
   }
+
+  console.log(code + 'oi')
 }
